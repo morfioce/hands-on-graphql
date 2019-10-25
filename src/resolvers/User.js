05b1ee496bb15cae5bb9c66472d9091dbe2a496e
Moncef <moncef.bouallagui@gmail.com>
2019-10-25 16:26:07 +0200

    Solution exercise 3

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,6 +1,10 @@
 const User = {
   tweets(parent, _, { db }) {
     return db.tweets.filter((t) => t.author == parent.id);
+  },
+
+  followers(parent, _, { db }) {
+    return db.users.filter((u) => u.follow.includes(parent.id));
   }
 };
 
